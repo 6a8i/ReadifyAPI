e5aa27e68b574ca27792b74eb9064f06f782d08d
Gabriele Bizzon <gbizzon@hotmail.com>
2025-04-07 16:28:59 -0300

    feat: developed feature to update a existing user.

---
+++ src/Readify.Application/Features/Users/V1/Models/Requests/UpdateUserRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Readify.Application.Features.Users.V1.Models.Requests
+{
+    public class UpdateUserRequest
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public bool? IsActive { get; set; }
+        public DateTime? BirthDate { get; set; }
+    }
+}
